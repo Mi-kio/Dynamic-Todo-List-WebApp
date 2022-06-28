c626ec5b64448f2e3ecb2be03369568a28f4788f
Mi-kio <sakshihere34@gmail.com>
2022-06-28 17:17:03 +0530

    multiple lists for multiple routes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //jshint esversion:6
+
+
 const express = require("express");
 const bodyparser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
-// var items= [];
-// var workItems= [];
 app.set("view engine","ejs");
 
 app.use(bodyparser.urlencoded({extended:true }));
@@ -29,21 +29,15 @@ const listitem3 = new Item({
 
 const defaultItems = [listitem1, listitem2, listitem3];
 
-
+const listSchema = {
+    name:String,
+    items:[itemsSchema]
+}
+const List = mongoose.model("List", listSchema);
 
 app.get("/",function(req,res){
-//     let today = new Date();
-//    let currentDay = today.getDay();
-//    let options={
-//        weekday:"long",
-//        month:"long",
-//        day:"numeric"
-//    };
-//    let day = today.toLocaleDateString("en-US",options);
 
 Item.find({},function(err,foundItems){
-    // console.log(foundItems);
-   
     if(foundItems.length === 0){
         Item.insertMany(defaultItems,function(err){
             if(err)
@@ -56,39 +50,54 @@ Item.find({},function(err,foundItems){
     else{
     res.render("list", {listTitle:"Today", 
     newlistitems:foundItems});
-
 }
 });
-   
-
-
-   
 });
+
+app.get("/:customListName",function(req,res){
+    const customListName = req.params.customListName;
+    List.findOne({name:customListName},function(err,foundList){
+if(!err){
+    if(!foundList){
+    const list = new List({
+        name:customListName,
+        items:defaultItems
+    });
+    list.save();
+    res.redirect("/" + customListName);
+}
+else{
+    res.render("list", {listTitle:foundList.name, 
+    newlistitems:foundList.items});
+
+}}
+})
+});
+
+
+
 
 app.post("/",function(req,res){
     const itemName = req.body.newItem;
+    const listName = req.body.list;
   const item = new Item({
     name:itemName
   });
-  item.save();
-  res.redirect("/");
-    // console.log(req.body);
-    // if(req.body.list === "Work"){
-    //      workItems.push(foundItems);
-    //      res.redirect("/work");
-    // }
-    // else{
-    //     items.push(foundItems);
-    //     res.redirect("/");
-    // } 
+
+  if(listName === "Today"){
+    item.save();
+    res.redirect("/");
+  }
+  else{
+    List.findOne({name:listName},function(err,foundList){
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/"+listName);
+    })
+  }
 });
 
 
-app.get("/work",function(req,res){
-    res.render("list",
-    {listTitle:"Work list",
-    newlistitems:workItems});
-});
 
 app.post("/delete",function(req,res){
 const checkedItemBody = req.body.checkbox;
@@ -101,20 +110,9 @@ const checkedItemBody = req.body.checkbox;
 
   })
 
-app.post("/work",function(req,res){
-    console.log(req.body);
-      let item = req.body.newItem;
-      workItems.push(item);
-      res.redirect("/work");
-  });
-
-  
-
   app.get("/about",function(req,res){
     res.send("I am Sakshi Jain. This is my first time using expressjs. I am loving this framework.");
     })
-
-
 
 app.listen(3000,function(){
     console.log("Server at post 3000");
